37af0e55d442c15df04c8d22468c32553b796905
satofumiya <sato.workfree@gmail.com>
2022-03-24 18:17:34 +0900

    make invoice class

--- Invoice.js
+++ Invoice.js
@@ -15,3 +15,23 @@ class InvoiceItem{
         return this.product.price * this.quantity;
     }
 }
+
+class Invoice{
+    constructor(invoiceNumber, invoiceItemHead){
+        this.invoiceNumber = invoiceNumber;
+        this.invoiceItemHead = invoiceItemHead;
+    }
+
+    amountDue(taxes){
+        this.invoiceItem = this.invoiceItemHead;
+        let allTotalPrice = 0;
+        
+        while(this.invoiceItem != null){
+            allTotalPrice += this.invoiceItem.getTotalPrice();
+            this.invoiceItem = this.invoiceItem.next;
+        }
+        
+
+        return(taxes) ? allTotalPrice*1.1 : allTotalPrice;
+    }
+}
